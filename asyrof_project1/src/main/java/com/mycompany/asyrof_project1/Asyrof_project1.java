d8685df64acd21ce382689e37d3354cc9eba1771
Asyrof14 <Acop1403@gmail.com>
2025-09-01 21:35:14 +0700

    2420506047_PBO_S3W2

---
+++ asyrof_project1/src/main/java/com/mycompany/asyrof_project1/Asyrof_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asyrof_project1;
+
+/**
+ *
+ * @author user
+ */
+public class Asyrof_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Muhammad_Asyrof");
+        System.out.println("NPM: 2420506047");
+        System.out.println("Alamat: Tidar Utara, Magelang Selatan");
+        System.out.println("No_HP: 081342882433");
+    }
+}
