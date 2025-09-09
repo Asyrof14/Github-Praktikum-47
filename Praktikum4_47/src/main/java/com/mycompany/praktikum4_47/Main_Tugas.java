11846034ec62dc0db637c98fad32573e6eadc6fa
Asyrof14 <Acop1403@gmail.com>
2025-09-09 21:04:57 +0700

    2420506047_PBO_S3W4

--- Praktikum4_47/src/main/java/com/mycompany/praktikum4_47/Main_Tugas.java
+++ Praktikum4_47/src/main/java/com/mycompany/praktikum4_47/Main_Tugas.java
@@ -10,20 +10,16 @@ package com.mycompany.praktikum4_47;
  */
 public class Main_Tugas {
     public static void main(String[] args) {
-        // Membuat objek dari kelas Pekerja
         Pekerja_Tugas pekerja1 = new Pekerja_Tugas("Raya Fitri", 20, "Software Engineer", 5000000.0);
-        
-        // Menampilkan informasi pekerja dengan toString()
+
         System.out.println("Informasi Awal:");
         System.out.println(pekerja1);
         System.out.println();
-        
-        // Mengubah nama pekerja menggunakan metode setter
+
         System.out.println("Informasi Nama, Gaji berubah");
         pekerja1.setNama("Raya Fitri Nirmala");
         pekerja1.setGaji(7000000.0);
-        
-        // Menampilkan ulang informasi pekerja setelah perubahan
+
         System.out.println("Setelah Perubahan:");
         System.out.println(pekerja1);
         System.out.println();
--- Praktikum4_47/src/main/java/com/mycompany/praktikum4_47/Pekerja_Tugas.java
+++ Praktikum4_47/src/main/java/com/mycompany/praktikum4_47/Pekerja_Tugas.java
@@ -12,25 +12,20 @@ public class Pekerja_Tugas extends Manusia_Tugas {
     private double gaji;
 
     public Pekerja_Tugas(String nama, int usia, String pekerjaan, double gaji) {
-        // Memanggil konstruktor dari kelas induk (Manusia)
         super(nama, usia, pekerjaan);
         this.gaji = gaji;
     }
     
-    // Getter untuk atribut 'gaji'
     public double getGaji() {
         return gaji;
     }
-    
-    // Setter untuk atribut 'gaji'
+
     public void setGaji(double gaji) {
         this.gaji = gaji;
     }
-    
-    // Override metode toString() untuk menampilkan informasi lengkap Pekerja
+
     @Override
     public String toString() {
-        // Menggunakan toString() dari kelas induk dan menambahkan informasi gaji
         return super.toString() + ", Gaji: " + this.gaji;
     }
 }
