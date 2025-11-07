5a231c11665f865ec36cae16c45ba01804b98ef6
Asyrof14 <Acop1403@gmail.com>
2025-11-07 07:58:12 +0700

    242050047_PBO_S3W9

---
+++ Praktikum9_47/src/main/java/com/mycompany/praktikum9_47/Kucing_Tugas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktikum9_47;
+
+/**
+ *
+ * @author user
+ */
+public class Kucing_Tugas extends Hewan_Tugas {
+    @Override
+    public void suara() {
+        System.out.println("Kucing mengeluarkan suara: Meong");
+    }
+}
---
+++ Praktikum9_47/src/main/java/com/mycompany/praktikum9_47/Main_Tugas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktikum9_47;
+
+/**
+ *
+ * @author user
+ */
+public class Main_Tugas {
+    public static void main(String[] args) {
+        Hewan_Tugas kucing = new Kucing_Tugas();
+        Hewan_Tugas anjing = new Anjing_Tugas();
+      
+        kucing.suara();
+        anjing.suara();
+    }
+}
